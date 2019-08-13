93972345d9a8f6450f45b1aab39f1dc08cc83a54
alexoshiro <alex.oshiro94@gmail.com>
2019-08-13 16:43:55 -0400

    Added possibility to choose which weekday to receive notification

--- telebot.py
+++ telebot.py
@@ -1,13 +1,11 @@
 import os
 import logging
 import datetime
-from telegram import ext, Bot
+from telegram import ext, Bot, InlineKeyboardButton, InlineKeyboardMarkup
 from pymongo import MongoClient
 from validate_email import validate_email
-#===Dev===
-#from dotenv import load_dotenv
-#load_dotenv()
-#===Dev===
+from dotenv import load_dotenv
+load_dotenv()
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -16,20 +14,21 @@ handler = logging.FileHandler('logs/bot.log', 'w', 'utf-8')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-# consoleHandler = logging.StreamHandler()
-# consoleHandler.setFormatter(formatter)
-# logger.addHandler(consoleHandler)
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(formatter)
+logger.addHandler(consoleHandler)
 client = None
 database = None
-
+weekdays_dictionary = {"0": "Segunda-Feira", "1": "Terça-Feira", "2": "Quarta-Feira", "3": "Quinta-Feira", "4": "Sexta-Feira", "5": "Sábado", "6": "Domingo"}
 command_list_message="\nLista de comandos:\n\
-        *cadastro* - visualizar seus dados cadastros em meu banco de dados\n\
+        */cadastro* ou *cadastro* - visualizar seus dados cadastros em meu banco de dados\n\
         */email (EMAIL_DE_CADASTRO)* - atualizar o email cadastrado\n\
         */cadastrar* - realizar um pré cadastro com suas informações, recuperadas pelo telegram\n\
         */planilha (NOME_NA_PLANILHA)* - realiza o cadastro de seu nome identificador na planilha de café da manhã, note que o essa informação deve ser igual a escrita na planilha\n\
         */telegram (on|off)* - altera configuração de alerta no telegram, *on* para começar a receber o alerta ou *off* para parar de receber o alerta\n\
-        */clear_email* - remove email do cadastro, com isso você para de receber alertas no email\n\
-        *ajuda* - visualizar a lista de comandos novamente.\
+        */limpar_email* - remove email do cadastro, com isso você para de receber alertas no email\n\
+        */dia* ou *dia* - atualizar o dia da semana que deseja receber as notificações.\
+        */ajuda* ou *ajuda* - visualizar a lista de comandos novamente.\
         "
 
 def getDatabase():
@@ -47,10 +46,18 @@ def getUserCollection():
 
 #client.close()
 def formatUserData(user):
-    return "Nome: {}\nEmail: {}\nIdentificador na planilha: {}\nAlerta no Telegram: {}\nCriado em: {}\nAtualizado em: {}".format(user['name'], 
+    global weekdays_dictionary
+    return "Nome: {}\
+        \nEmail: {}\
+        \nIdentificador na planilha: {}\
+        \nNotificação no Telegram: {}\
+        \nDia da semana para notificação: {}\
+        \nCriado em: {}\
+        \nAtualizado em: {}".format(user['name'], 
         (user['email'] if 'email' in user and user['email'] != None else ""),
         (user['spreadsheet_identifier'] if 'spreadsheet_identifier' in user and user['spreadsheet_identifier'] != None else ""),
         ("Ativo" if 'alert_telegram' in user and user['alert_telegram'] else "Inativo"),
+        (weekdays_dictionary.get(user['alert_weekday']) if 'alert_weekday' in user and user['alert_weekday'] != None else ""),
         (user['created_at'].strftime("%d/%m/%Y %H:%M:%S") if 'created_at' in user and user['created_at'] != None else ""),
         (user['updated_at'].strftime("%d/%m/%Y %H:%M:%S") if 'updated_at' in user and user['updated_at'] != None else "")
     )
@@ -67,6 +74,8 @@ def signup(bot, update):
             "spreadsheet_identifier":update.message.from_user.first_name,  
             "chat_id": str(update.message.chat_id),
             "alert_telegram": False,
+            "alert_weekday": 3,
+            "alert_hour": "15:00",
             "created_at": datetime.datetime.now(),
             "updated_at": datetime.datetime.now(),
         }
@@ -182,6 +191,8 @@ def text_decoder(bot, update):
         signup(bot, update)
     elif user_text == "ajuda":
         help(bot, update)
+    elif user_text == "dia":
+        show_week_day_keyboard(bot, update)
     else:
         is_valid = validate_email(update.message.text)
         if(is_valid):
@@ -202,17 +213,73 @@ def text_decoder(bot, update):
             text=message
         )
 
+def build_menu(buttons, n_cols, header_buttons=None, footer_buttons=None):
+    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+    if header_buttons:
+        menu.insert(0, [header_buttons])
+    if footer_buttons:
+        menu.append([footer_buttons])
+    return menu
+
+def update_notification_weekday(user_id, choosen_weekday):
+    global weekdays_dictionary
+    message = "Configuração atualizada."
+    if(choosen_weekday != None and choosen_weekday != "" and weekdays_dictionary.get(choosen_weekday) != None) :
+        user_collection = getUserCollection()
+        result = user_collection.update_one({"_id": str (user_id)},  {"$set": {"alert_weekday": choosen_weekday, "updated_at": datetime.datetime.now()}})
+        if(result.modified_count <= 0):
+            message = "Não foi encontrar o cadastro a ser atualizado, por favor tente novamente mais tarde."
+    else:
+        message = "Opção informada é inválida"
+    return message
+
+def show_week_day_keyboard(bot, update):
+    button_list = [
+        InlineKeyboardButton("Segunda-Feira", callback_data="weekday=0"),
+        InlineKeyboardButton("Terça-Feira", callback_data="weekday=1"),
+        InlineKeyboardButton("Quarta-Feira", callback_data="weekday=2"),
+        InlineKeyboardButton("Quinta-Feira", callback_data="weekday=3"),
+        InlineKeyboardButton("Sexta-Feira", callback_data="weekday=4"),
+        InlineKeyboardButton("Sábado", callback_data="weekday=5"),
+        InlineKeyboardButton("Domingo", callback_data="weekday=6")
+    ]
+    reply_markup = InlineKeyboardMarkup(build_menu(button_list, n_cols=2))
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        text="Escolha um dia da semana para atualizar",
+        reply_markup=reply_markup
+    )
+
+def callback_handler(bot, update):
+    message = "Configuração atualizada."
+    query = update.callback_query
+    if("weekday" in query.data):
+        message = update_notification_weekday(query.message.chat.id,query.data.replace("weekday=", "") )
+    bot.answer_callback_query(
+        callback_query_id=query.id,
+        text=message
+    )
+    bot.send_message(
+        chat_id=query.message.chat.id,
+        text=message
+    )
+    bot.deleteMessage(
+        chat_id=query.message.chat.id,
+        message_id=query.message.message_id
+    )
 def main():
     updater = ext.Updater(os.environ['TELEGRAM_BOT_KEY'])
     dispatcher = updater.dispatcher
-    #dispatcher.add_handler(ext.CommandHandler('cadastro', register))
+    dispatcher.add_handler(ext.CommandHandler('cadastro', register))
     dispatcher.add_handler(ext.CommandHandler('start', hello))
     dispatcher.add_handler(ext.CommandHandler('cadastrar', signup))
     dispatcher.add_handler(ext.CommandHandler('email', update_email, pass_args=True))
     dispatcher.add_handler(ext.CommandHandler('planilha', register_spreadsheet_name, pass_args=True))
     dispatcher.add_handler(ext.CommandHandler('telegram', change_alert_telegram, pass_args=True))
-    dispatcher.add_handler(ext.CommandHandler('clear_email', clear_email))
+    dispatcher.add_handler(ext.CommandHandler('limpar_email', clear_email))
     dispatcher.add_handler(ext.CommandHandler('ajuda', help))
+    dispatcher.add_handler(ext.CommandHandler('dia', show_week_day_keyboard))
+    dispatcher.add_handler(ext.CallbackQueryHandler(callback_handler))
     dispatcher.add_handler(ext.MessageHandler(ext.Filters.text, text_decoder))
     logger.info("Starting polling")
     updater.start_polling()
